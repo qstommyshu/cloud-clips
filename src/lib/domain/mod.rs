2765ae04c3139000826e561993667d5b77aca679
qstommyshu <qstommyshu@gmail.com>
2023-07-24 21:56:19 -0400

    create time struct

---
+++ src/lib/domain/mod.rs
@@ -0,0 +1,2 @@
+pub mod clip;
+pub mod time; //re-export time in this module
---
+++ src/lib/domain/time.rs
@@ -0,0 +1,35 @@
+use chrono::{ DateTime, NaiveDateTime, Utc };
+use derive_more::From;
+use serde::{ Deserialize, Serialize };
+use std::std::FromStr;
+
+#[derive(Clone, Debug, From, Deserialize, Serialize)]
+// Benefit of wrapping DateTime into Time Struct: breaking update in DateTime trait will can be
+// limited to change only in Time struct we created, and we can use derive to add traits to Time.
+pub struct Time(DateTime<Utc>);
+
+impl Time {
+    pub fn into_inner(self) -> DateTime<Utc> {
+        self.0
+    }
+
+    pub fn timestamp(&self) -> i64 {
+        self.0.timestamp()
+    }
+
+    pub from_naive_utc(datetime: NaiveDateTime) -> Self {
+        Time(DateTime::from_utc(datetime, Utc))
+    }
+}
+
+
+impl FromStr for Time {
+    type Err = chrono::ParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // 2022-05-22 to DateTime struct
+        match format!("{s}T00:00:00Z").parse::<DateTime<Utc>>() {
+            Ok(time) => Ok(time.into()),
+            Err(e) => Err(e)
+        }
+    }
+}
